edee6886e89209a3158d4a8dfdd7103a965f757d
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-09 00:41:57 -0300

    Area_Circulo.cs

---
+++ Area_Circulo.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Area_Circulo.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = 3.1415;
+            Console.Write("Ingrese el radio del circulo: ");
+            int radio = Convert.ToInt32(Console.ReadLine());
+
+            area = 3.1415 * radio * 2;
+
+            Console.WriteLine("El area del circulo es: " + area);
+
+
+            Console.ReadKey();
+        }
+    }
+}
